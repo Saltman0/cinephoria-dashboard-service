0bf19a50466e441e2e011594b6eccf9b291871fc
Saltman0 <baudoin.mathieu@protonmail.com>
2025-12-17 01:44:06 +0100

    🔥 Database config removed

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import cors from "cors";
 import express, { Express } from "express";
-import databaseConfig from "./config/database.config.ts";
 import dashboardRoutes from "./routes/dashboard.routes.ts";
 
 export const port: number = parseInt(process.env.PORT as string) || 3006;
@@ -9,5 +8,4 @@ export const app: Express = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(databaseConfig);
 app.use(dashboardRoutes);
