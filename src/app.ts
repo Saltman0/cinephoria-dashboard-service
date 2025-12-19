5f29c28e495fbbae3616d132cb29f7e2cb9cc0c8
Saltman0 <baudoin.mathieu@protonmail.com>
2025-12-19 14:43:43 +0100

    ✨ Rabbit MQ configuration added

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import cors from "cors";
 import express, { Express } from "express";
 import dashboardRoutes from "./routes/dashboard.routes.ts";
+import { subscribeToMessages } from "./rabbitmq";
 
 export const port: number = parseInt(process.env.PORT as string) || 3006;
 
@@ -9,3 +10,4 @@ export const app: Express = express();
 app.use(cors());
 app.use(express.json());
 app.use(dashboardRoutes);
+await subscribeToMessages("bookingHistory");
---
+++ src/rabbitmq.ts
@@ -0,0 +1,28 @@
+import * as amqp from "amqplib";
+import { eventEmitter } from "./events/events";
+
+const url: string = "amqp://"+process.env.RABBITMQ_USER+":"+process.env.RABBITMQ_PASSWORD+"@"+process.env.RABBITMQ_IP;
+
+export async function subscribeToMessages(exchange: string): Promise<void> {
+    try {
+        const connection: amqp.Connection = await amqp.connect(url);
+        const channel: amqp.Channel = await connection.createChannel();
+
+        await channel.assertExchange(exchange, "fanout", { durable: false });
+
+        const { queue } = await channel.assertQueue('', { exclusive: true });
+
+        await channel.bindQueue(queue, exchange, '');
+
+        // Consume messages from the queue
+        await channel.consume(queue, function(message: amqp.ConsumeMessage|null) {
+            if (message !== null) {
+                eventEmitter.emit("messageReceived", JSON.parse(message.content.toString()));
+            }
+        }, {
+            noAck: true
+        });
+    } catch (error) {
+        console.error(error);
+    }
+}
