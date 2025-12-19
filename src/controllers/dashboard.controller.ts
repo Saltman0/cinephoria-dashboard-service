621a6089297afab0651fd587b8e24a68c3b662dc
Saltman0 <baudoin.mathieu@protonmail.com>
2025-12-19 16:00:26 +0100

    ✨ Dashboard controller : updateBookingHistory method updated

--- src/controllers/dashboard.controller.ts
+++ src/controllers/dashboard.controller.ts
@@ -33,12 +33,13 @@ export async function createBookingHistory(req: Request, res: Response) {
 
 export async function updateBookingHistory(req: Request, res: Response) {
     try {
-        const bookingHistory = await dashboardRepository.updateBookingHistory(
+        await dashboardRepository.updateBookingHistory(
+            req.params.bookingHistoryId,
             req.body.bookingId,
             req.body.date
         );
 
-        res.status(200).json(bookingHistory.insertedId.toString());
+        res.status(200).json({ message: "Booking history updated successfully." });
     } catch (error) {
         if (error instanceof Error) {
             res.status(500).json({message: error.message});
