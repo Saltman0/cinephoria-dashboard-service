7823d9f15a105261c51ba9b7b6a399b6b0fc5dd8
Saltman0 <baudoin.mathieu@protonmail.com>
2025-12-28 18:28:22 +0100

    📦️ app.ts updated

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import cors from "cors";
 import express, { Express } from "express";
+import pino from "pino";
 import dashboardRoutes from "./routes/dashboard.routes.ts";
 import { subscribeToMessages } from "./rabbitmq.ts";
 
@@ -7,7 +8,17 @@ export const port: number = parseInt(process.env.PORT as string) || 3006;
 
 export const app: Express = express();
 
+export const logger = pino({
+    transport: {
+        target: "pino-pretty",
+        options: {
+            colorize: true
+        }
+    }
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(dashboardRoutes);
+
 await subscribeToMessages("bookingHistory");
